e2a4e0123b63b8f28ff0ea2747e36abffb5d04ea
TMac-Liu <tina@e.hunantv.com>
2018-04-25 22:44:56 +0800

    x

--- template/src/index.js
+++ template/src/index.js
@@ -7,8 +7,6 @@ import Store from './store';
 import router from './routers';
 import Utils from './utils';
 // import './element-variables.scss';
-import './styles/iconfont/iconfont.css';
-import './styles/main.scss';
 
 Vue.use(ElementUI);
 Vue.use(Utils);
